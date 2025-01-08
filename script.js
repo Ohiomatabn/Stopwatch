d5e674148b6c2d8783111dc47e978548fb7d43de
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-01-08 21:02:24 +0100

    Make the reset button interactive

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ function stop(){
     isRunning = false;
   }
 }
+
+
+function reset(){
+  clearInterval(timer);
+  let startTime = 0;
+  let elapsedTime = 0;
+  let isRunning = false;
+  display.textContent = "00:00:00:00"
+
+  
+}
+
+
+function update(){
+
+  const currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
+
+  let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+  let seconds = Math.floor(elapsedTime / 1000 % 60);
+  let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+  hours = String(hours).padStart(2, 0);
+  minutes = String(minutes).padStart(2, 0);
+  seconds = String(seconds).padStart(2, 0);
+  milisecondsm = String(miliseconds).padStart(2, 0);
+
+  display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`
+}
